72842cf0b6fe1631989f4bb1ad3849a254f0ac76
huangwenda <417198820@qq.com>
2019-11-14 10:50:19 +0800

    补充文件

---
+++ compareData.py
@@ -0,0 +1,61 @@
+import mysqlExecute
+import myDocx
+
+
+class DocxInfo:
+    def __init__(self, document_path):
+        self.docx_info = myDocx.MyDocx(document_path=document_path)
+        self.table_info_list = self.docx_info.get_info_list()
+
+
+
+'''
+输出的docx信息格式：（依据数据字典模板1.2定）
+{'table_name': 'report',
+ 'params': {'report_id': {'type': 'Int(8)', 'is_null': 'n', 'is_only': 'y', 'is_index': 'y', 'is_null_ori': 'n', 'is_only_ori': 'y', 'is_index_ori': 'y', 'comment': '自增主键id，从10000开始自增。系统生成的报告编号。', 'default': '', checked:'no'},
+            'result': {'type': 'tinyint(1)', 'is_null': 'n', 'is_only': 'n', 'is_index': 'n', 'is_null_ori': 'n', 'is_only_ori': 'y', 'is_index_ori': 'y', 'comment': '巡检记录是n存在异常；系统自动生成，巡检结果；0正常，1异常', 'default': '0', checked:'no'},
+            'creator': {'type': 'varchar(16)', 'is_null': 'n', 'is_only': 'n', 'is_index': 'n', 'is_null_ori': 'n', 'is_only_ori': 'y', 'is_index_ori': 'y', 'comment': '创建人', 'default': '', checked:'no'},
+            'create_time': {'type': 'timestamp', 'is_null': 'n', 'is_only': 'n', 'is_index': 'n', 'is_null_ori': 'n', 'is_only_ori': 'y', 'is_index_ori': 'y', 'comment': '创建时间', 'default': 'CURRENT_TIMESTAMP', checked:'no'},
+            'updater': {'type': 'varchar(16)', 'is_null': 'n', 'is_only': 'n', 'is_index': 'n', 'is_null_ori': 'n', 'is_only_ori': 'y', 'is_index_ori': 'y', 'comment': '编辑人', 'default': '', checked:'no'},
+            'update_time': {'type': 'timestamp', 'is_null': 'n', 'is_only': 'n', 'is_index': 'n', 'is_null_ori': 'n', 'is_only_ori': 'y', 'is_index_ori': 'y', 'comment': '编辑时间', 'default': 'CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP', checked:'no'}}
+'database_engine': 'InnoDB',
+'default_charset': 'utf8',
+'union_index_ori': 'Aaa(dsadsa,dsads),dsad_sa(dsads,dsadsa)',
+'union_index': {'Aaa': ['dsads', 'dsadsa'], 'dsad_sa': ['dsads', 'dsadsa']}
+}
+'''
+
+
+class MysqlInfo:
+    def __init__(self, host=None, port=3306, user=None, password=None, db=None, charset='utf8'):
+        self.mysql_info = mysqlExecute.MysqlExecute(host=host, port=port, user=user, password=password, db=db, charset=charset)
+
+    def get_table_info(self, table_name):
+        return self.mysql_info.make_table_info(table_name)
+
+
+'''
+输出的mysql信息格式：（依据数据字典模板1.2定）
+{'param_info': {'id': {'type': 'int(11)', 'is_null': 'n', 'is_only': 'y', 'is_index': 'PRI', 'auto_increment': 'y', default:'', 'comment': '主键', checked:'no'},
+                'schedule_code': {'type': 'char(23)', 'is_null': 'n', 'is_only': 'y', 'is_index': 'UNI', default:'', 'comment': '排期编号 投放策略编号+3位数递增', checked:'no'},
+                'date': {'type': 'date', 'is_null': 'n', 'is_only': 'n', 'is_index': '', default:'', 'comment': '投放日期, checked:'no''},
+                'amount': {'type': 'varchar(11)', 'is_null': 'n', 'is_only': 'n', 'is_index': 'MUL', default:'', 'comment': '在该日期的投放量,单位次', checked:'no'},
+                'create_time': {'type': 'timestamp', 'is_null': 'n', 'is_only': 'n', 'is_index': '',  default:'','comment': '创建时间', checked:'no'},
+                'creator': {'type': 'varchar(32)', 'is_null': 'n', 'is_only': 'n', 'is_index': '', default:'', 'comment': '创建人', checked:'no'},
+                'update_time': {'type': 'timestamp', 'is_null': 'n', 'is_only': 'n', 'is_index': '', default:'', 'comment': '更新时间', checked:'no'},
+                'updater': {'type': 'varchar(32)', 'is_null': 'n', 'is_only': 'n', 'is_index': '', default:'', 'comment': '修改人', checked:'no'}, 
+                'test': {'type': 'geomcollection', 'is_null': 'n', 'is_only': 'n', 'is_index': 'MUL' default:'',}},
+'table_name': 'schedule_daily_schedule',
+'database_engine': 'InnoDB',
+'default_charset': 'utf8',
+'database_engine_ori': 'InnoDB',
+'default_charset_ori': 'utf8',
+'union_index_ori':'',
+'comment': '排期每日排期表'
+'union_index':{'PRIMARY_KEY': ['id', 'schedule_code'], 'schedule_code': ['schedule_code', 'amount']}
+}
+'''
+
+if __name__ == '__main__':
+    qq = MysqlInfo(host='127.0.0.1', user='root', password='123456', db='mytest')
+    qq.get_table_info('schedule_daily_schedule')
